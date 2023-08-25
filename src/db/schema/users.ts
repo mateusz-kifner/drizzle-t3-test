258840e94038160fcd30841aa9f7e617874756db
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-08-26 00:10:52 +0200

    Added drizzle-zod

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -3,7 +3,7 @@ import { metadata } from "./_metadata";
 import { orders_to_users } from "./orders_to_users";
 import { relations } from "drizzle-orm";
 import { email_credentials_to_users } from "./email_credentials_to_users";
-
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: text("name"),
@@ -19,6 +19,10 @@ export const users_relations = relations(users, ({ one, many }) => ({
   orders: many(orders_to_users),
   emailCredentials: many(email_credentials_to_users),
 }));
+
+export const insertUserSchema = createInsertSchema(users);
+
+export const selectUserSchema = createSelectSchema(users);
 
 export type User = typeof users.$inferSelect; // return type when queried
 export type NewUser = typeof users.$inferInsert; // insert type
